0550ccfd83d907c15f0c6544a1fb2c3c14c82465
Jinwoo Park <zvzv9808@gmail.com>
2023-03-23 02:26:26 +0900

    Initial Commit

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController], //url을 가져오고 함수를 실행
+  providers: [AppService],
+})
+export class AppModule {}
