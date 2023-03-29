c73b016a824a7bbbce62fdd578d41aa6d2951cf1
Jinwoo Park <zvzv9808@gmail.com>
2023-03-29 20:52:26 +0900

    Complete Chapter 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true, //한번 Validation에 걸리면 이후 데이터는 검증하지 않음.
-    forbidNonWhitelisted: true//이상한 데이터가 넘어오면, request 자체를 막아버림.
+    forbidNonWhitelisted: true,//이상한 데이터가 넘어오면, request 자체를 막아버림.
+    transform: true //request로 들어온 값을 실제 값으로 변환
   }))
   await app.listen(3000);
 }
